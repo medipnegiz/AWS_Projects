5383257711f15c2f82f0eb98b4c5e376f83d804b
Mahmut Edip Negiz <100766962+medipnegiz@users.noreply.github.com>
2022-05-25 10:29:33 +0200

    ilk dosya eklendi

---
+++ flask-01-02-hello-world-app-Jinja-Template/flask-01-hello-world-app/hello-world-app.py
@@ -0,0 +1,22 @@
+from flask import Flask 
+
+app = Flask(__name__)
+
+@app.route("/")
+def head():
+    return "Hello world from Flask"
+
+@app.route("/second")
+def second():
+    return "This is my second page"
+
+@app.route("/third/subthird")
+def third():
+    return "<center><h2>This is the subpath of third page</h2></center>"  
+
+@app.route("/forth/<string:id>")
+def forth(id):
+    return f'<h2>Id of this page is {id}</h2>' 
+
+if __name__ == "__main__":
+    app.run(debug=True, port=80)    # default olarak 5000 port gelir ama istedigimiz gibi tanimlayabiliriz
